9fb2260e2170ae181d9cdfc9178948c809aa70b9
Jonathan Ziesmer <jonathan@ziesmer.us>
2018-11-19 10:44:31 -0500

    date display range works

--- calendar_ui/Assets/Scripts/DayLayout.cs
+++ calendar_ui/Assets/Scripts/DayLayout.cs
@@ -1,7 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 using UnityEngine.UI;
+using Debug = UnityEngine.Debug;
 
 public class DayLayout : MonoBehaviour
 {
@@ -13,10 +18,14 @@ public class DayLayout : MonoBehaviour
 	
 	//should be set to Arial
 	public Font tfon;
+
+	private DateObj.DateObject startDate;
+	private DateObj.DateObject today;
+	// should be set to the DateText GameObject
+	public GameObject placeholder;
 	
 	// Use this for initialization
 	void Start () {
-		
 		//make vertical lines to divide the days
 		for (int i = 0; i < 6; i++)
 		{
@@ -40,7 +49,27 @@ public class DayLayout : MonoBehaviour
 			a.transform.position = new Vector3(517 + i * 388, 1235, 0);
 			a.GetComponent<RectTransform>().sizeDelta = new Vector2(10, 156);
 		}
-
+		
+		//Make panel to highlight the current day
+		GameObject b = new GameObject("Today");
+		
+		//add necessary components
+		b.AddComponent<RectTransform>();
+		b.AddComponent<CanvasRenderer>();
+		b.AddComponent<Image>();
+		
+		//edit Image component
+		Image bimg = b.GetComponent<Image>();
+		bimg.sprite = img;
+		bimg.color = new Color(1, 0, 0, 255);
+		bimg.type = Image.Type.Sliced;
+		bimg.fillCenter = true;
+		
+		//set location
+		b.transform.SetParent(parent.transform);
+		b.transform.position = new Vector3(322.5f, 1224, 0);
+		b.GetComponent<RectTransform>().sizeDelta = new Vector2(382, 120);
+		
 		string[] days = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
 		for (int i = 0; i < days.Length; i ++)
 		{
@@ -51,21 +80,32 @@ public class DayLayout : MonoBehaviour
 			a.AddComponent<Text>();
 
 			Text t = a.GetComponent<Text>();
-			t.text = days[i];
 			t.font = tfon;
-			t.fontSize = 60;
+			t.fontSize = 50;
 			t.fontStyle = FontStyle.Bold;
 			t.color = new Color(0, 0, 0, 255);
 			t.alignment = TextAnchor.UpperCenter;
 
 			a.transform.SetParent(parent.transform);
-			a.transform.position = new Vector3(324 + i * 388, 1221, 0);
-			a.GetComponent<RectTransform>().sizeDelta = new Vector2(140, 80);
+			a.transform.position = new Vector3(324 + i * 388, 1216, 0);
+			a.GetComponent<RectTransform>().sizeDelta = new Vector2(398, 130);
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//get startDate from date.cs
+		startDate = placeholder.GetComponent<date>().display_date.week_start();
 		
+		Text[] children = gameObject.transform.GetComponentsInChildren<Text>(true);
+		for (int i = 0; i < children.Length; i ++)
+		{
+			DateObj.DateObject t = startDate + new DateObj.DateObject(0, 0, i);
+			
+			children[i].text = t.ToString("D") + "\n" + t.ToString("d:mm:yy");
+		}
+
+		today = placeholder.GetComponent<date>().today;
+		gameObject.transform.Find("Today").transform.position = new Vector3(322.5f + (today.day_of_the_week() - 1) * 388, 1224, 0);
 	}
 }
--- calendar_ui/Assets/Scripts/date.cs
+++ calendar_ui/Assets/Scripts/date.cs
@@ -4,6 +4,7 @@ using System.Runtime.InteropServices;
 using UnityEngine;
 using System;
 using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -14,121 +15,15 @@ public class date : MonoBehaviour {
 	public int month;
 	public int year;
 
-	public DateTime current_time;
-
-	private Text this_text;
-
-	private readonly string[] months = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-	private readonly string[] month_abrvs = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov"};
-
-	//returns length of specified month in days
-	private int month_len(int mon, int yr)
-	{
-		int ml = 0;
-
-		if (mon == 1)
-		{
-			ml = 31;
-		}
-
-		if (mon == 2)
-		{
-			ml = (yr % 4 == 0) ? 29 : 28;
-		}
-
-		if (mon == 3)
-		{
-			ml = 31;
-		}
-
-		if (mon == 4)
-		{
-			ml = 30;
-		}
-
-		if (mon == 5)
-		{
-			ml = 31;
-		}
-
-		if (mon == 6)
-		{
-			ml = 30;
-		}
-
-		if (mon == 7)
-		{
-			ml = 31;
-		}
-
-		if (mon == 8)
-		{
-			ml = 31;
-		}
-
-		if (mon == 9)
-		{
-			ml = 30;
-		}
-
-		if (mon == 10)
-		{
-			ml = 31;
-		}
-
-		if (mon == 11)
-		{
-			ml = 30;
-		}
-
-		if (mon == 12)
-		{
-			ml = 31;
-		}
-
-		return ml;
-	}
+	public DateObj.DateObject today;
 	
-	private readonly string[] days = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-	private readonly string[] day_abrvs = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
-
-	//doesn't allow for going before 2018
-	private int total_days (int day, int mon, int yr)
-	{
-		int num_days = day;
-		
-		for (int i = 0; i < yr - 2018; i++)
-		{
-			num_days += ((i + 2) % 4 == 0) ? 366 : 365;
-		}
-
-		for (int i = 1; i < mon; i++)
-		{
-			num_days += month_len(i - 1, yr);
-		}
-
-		return num_days;
-	}
-
-	private int total_days()
-	{
-		return total_days(day, month, year);
-	}
-	
-	private string date_to_day(int day, int mon, int yr)
-	{
-		return days[total_days(day, mon, yr) % 7];
-	}
-
-	private int week_start;
-	private int week_end;
-	
+	//allows date of first day currently being displayed to be accessed by other scripts
+	public DateObj.DateObject display_date;
 	
 	//used for last week button
 	public void last_week()
 	{
-		week_start -= 7;
-		week_end -= 7;
+		display_date -= new DateObj.DateObject(0, 0, 7);
 		
 		//update displayed date ranges
 		
@@ -138,21 +33,17 @@ public class date : MonoBehaviour {
 	//used for next week button
 	public void next_week()
 	{
-		week_start += 7;
-		week_end += 7;
-		
+		display_date += new DateObj.DateObject(0, 0, 7);
+
 		//update displayed date ranges
-		
+
 		//get events from database
 	}
 
 	//used for this week button, also updating week-dependent variables
 	public void this_week()
 	{
-		week_start = total_days() - total_days() % 7 + 1;
-		week_end = week_start + 6;
-		
-		//update displayed date ranges
+		display_date = today.week_start();
 		
 		//get events from database
 	}
@@ -173,19 +64,15 @@ public class date : MonoBehaviour {
 		SceneManager.LoadScene("Login");
 	}
 	
-	//
-	public int week_start_date()
-	{
-		int a = week_start;
-		for(int i = month; )
-	}
 	
 	// Use this for initialization
 	void Start ()
 	{
-		current_time = DateTime.Now;
+		DateTime ct = DateTime.Now;
+		today = new DateObj.DateObject(ct.Year, ct.Month, ct.Day);
+		display_date = today.week_start();
 
-		this_text = GetComponent<Text>();
+		GetComponent<Text>().text = today.ToString("d:M:Y");
 
 		this_week();
 	}
@@ -193,20 +80,16 @@ public class date : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
 	{
-		DateTime new_time = DateTime.Now;
-
-		if (new_time.Day != current_time.Day)
+		//check to see if it's a new day yet
+		DateTime ct = DateTime.Now;
+		
+		if (ct.Year != today.year || ct.Month != today.month || ct.Day != today.day)
 		{
-			day += 1;
-
-			if (total_days(day, month, year) > week_end)
-			{
-				this_week();
-			}
+			today = new DateObj.DateObject(ct.Year, ct.Month, ct.Day);
+			
+			GetComponent<Text>().text = today.ToString("d:M:Y");
 		}
 
-		current_time = new_time;
-		
-		//display events
+		//display events here or in CalendarLayout.cs?
 	}
 }
